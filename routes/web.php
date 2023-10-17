91f6ded941f917350b5105ffd7e0baf84b72c3c5
Abisarafi <Aubinskh123@student.uns.ac.id>
2023-10-17 14:48:03 +0700

    Penambahan layout kalender

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Auth; // Import the Auth facade
 use App\Http\Controllers\PengajuanController;
 use App\Http\Controllers\AdminController;
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -63,4 +67,22 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
 Route::get('/calendar', function () {
     return view('calendar');
+});
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/test', [UserController::class, 'test']);
+
+// Calendar routes
+Route::get('calendar/index', [CalendarController::class, 'index'])->name('calendar.index');
+Route::post('calendar', [CalendarController::class, 'store'])->name('calendar.store');
+Route::post('postPengajuan', [CalendarController::class, 'storePengajuan'])->name('calendar.storePengajuan');
+Route::patch('calendar/update/{id}', [CalendarController::class, 'update'])->name('calendar.update');
+Route::delete('calendar/destroy/{id}', [CalendarController::class, 'destroy'])->name('calendar.destroy');
+Route::delete('pengajuan/destroy/{id}', [CalendarController::class, 'destroyPengajuan'])->name('calendar.destroyPengajuan');
+
+Route::get('books', [BookController::class, 'index'])->name('books.index');
+Route::post('books', [BookController::class, 'store'])->name('books.store');
+
+Route::get('/export-db', function() {
+    return "Exporting";
 });
