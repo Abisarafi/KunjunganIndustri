a496591bf47916156794f08c7102fda304ee2bf5
Abisarafi <Aubinskh123@student.uns.ac.id>
2023-11-20 09:42:48 +0700

    memperbaiki beberapa fitur

--- resources/views/calendar/index2.blade.php
+++ resources/views/calendar/index2.blade.php
@@ -109,8 +109,8 @@
             </select>
             <select class="form-control" id="participant_count" name="participant_count" required>
                 <option value="" disabled selected>Jumlah Kelas (1 kelas max 30 orang)</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
+                <option value="1">1 Kelas</option>
+                <option value="2">2 Kelas</option>
             </select>
         </div>
         <div class="modal-footer">
