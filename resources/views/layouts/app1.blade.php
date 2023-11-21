ebf73e0e500810fe70f5e0efb4e9f0b8194e0153
hrzaa <harezayoan@gmail.com>
2023-11-21 11:06:45 +0700

    change logo

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -43,8 +43,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../index3.html" class="brand-link">
-      <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <img src="{{ asset('assets/images/logo_cleon.png') }}" alt="AdminLTE Logo" class="brand-image">
+      <span class="brand-text font-weight-light">Panel Admin</span>
     </a>
 
     <!-- Sidebar -->
