b8703b0fc7434086c3296611c427b70494efa51b
Abisarafi <Aubinskh123@student.uns.ac.id>
2023-11-30 22:53:21 +0700

    seeder admin

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User; // Adjust the namespace based on your actual model location
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Seed an admin user
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'noHP' => '987973321',
+            'email_verified_at' => now(),
+            'password' => Hash::make('adminpassword123'),
+        ]);
+
+        // You can add more users as needed
     }
 }
